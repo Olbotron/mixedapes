e334990fcbd0eed6f2319dd10d5b51e61d19e6fa
Olbotron <oliver.g.hughes@gmail.com>
2025-03-17 12:07:21 +0000

    Update static files config

--- tape/forms.py
+++ tape/forms.py
@@ -8,11 +8,38 @@ class TapeForm(forms.ModelForm):
         model = Tape
         fields = ['title', 'description']
 
+class AddSongForm(forms.ModelForm):
+    class Meta:
+        model = Song
+        fields = ['title', 'artist', 'album', 'album_art_url']
+
+class EditSongForm(forms.ModelForm):
+    class Meta:
+        model = Song
+        fields = ['title', 'artist', 'album', 'album_art_url']
+
+class DeleteSongsForm(forms.Form):
+    songs = forms.ModelMultipleChoiceField(
+        queryset=Song.objects.none(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False
+    )
+
+    def __init__(self, *args, **kwargs):
+        queryset = kwargs.pop('queryset', None)
+        super().__init__(*args, **kwargs)
+        if queryset is not None:
+            self.fields['songs'].queryset = queryset
+
+    def save(self):
+        songs_to_delete = self.cleaned_data['songs']
+        songs_to_delete.delete()
+
 class AddSongToTapeForm(forms.Form):
     title = forms.CharField(max_length=100)
     artist = forms.CharField(max_length=100)
 
-AddSongToTapeFormSet = forms.formset_factory(AddSongToTapeForm, extra=3)  # Adjust 'extra' to the number of forms you want to display
+AddSongToTapeFormSet = formset_factory(AddSongToTapeForm, extra=3)  # Adjust 'extra' to the number of forms you want to display
 
 class DeleteSongsFromTapeForm(forms.Form):
     songs = forms.ModelMultipleChoiceField(
