ccbbc611ebe24bc414aa40cf7d692ea27fefaa61
Olbotron <oliver.g.hughes@gmail.com>
2025-03-17 13:56:39 +0000

    Fixing heroku deployment

--- playlist_app/urls.py
+++ playlist_app/urls.py
@@ -1,13 +1,12 @@
+from django.contrib import admin
 from django.urls import path, include
 from tape import views as tape_views
 
 urlpatterns = [
-    path('', tape_views.tape_list, name='tape_list'),
-    path('<int:pk>/', tape_views.tape_detail, name='tape_detail'),
-    path('new/', tape_views.tape_create, name='tape_create'),
-    path('<int:pk>/edit/', tape_views.tape_update, name='tape_update'),
-    path('<int:pk>/delete/', tape_views.tape_delete, name='tape_delete'),
-    path('<int:pk>/add_song/', tape_views.add_song_to_tape, name='add_song_to_tape'),
-    path('<int:pk>/delete_songs/', tape_views.delete_songs_from_tape, name='delete_songs_from_tape'),
-    path('song/<int:pk>/edit/', tape_views.edit_song, name='edit_song'),
+    path('admin/', admin.site.urls),
+    path('', tape_views.home, name='home'),  # Use the home view from the tape app
+    path('tapes/', include('tape.urls')),
+    path('songs/', include('song.urls')),  # Include the song app's URLs
+    path('users/', include('user.urls')),  # Include the user app's URLs
+    path('accounts/', include('django.contrib.auth.urls')),  # Include the authentication URLs
 ]
