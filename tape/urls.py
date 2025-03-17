b8cd0bb9fd89ca48c22a5f5ce58427527e2b96c4
Olbotron <oliver.g.hughes@gmail.com>
2025-03-17 13:11:19 +0000

    Update static files config

--- tape/urls.py
+++ tape/urls.py
@@ -3,11 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.tape_list, name='tape_list'),
-    path('tapes/', views.tape_detail, name='tape_detail'),
+    path('<int:pk>/', views.tape_detail, name='tape_detail'),
     path('new/', views.tape_create, name='tape_create'),
     path('<int:pk>/edit/', views.tape_update, name='tape_update'),
     path('<int:pk>/delete/', views.tape_delete, name='tape_delete'),
     path('<int:pk>/add_song/', views.add_song_to_tape, name='add_song_to_tape'),
     path('<int:pk>/delete_songs/', views.delete_songs_from_tape, name='delete_songs_from_tape'),
-    path('song/<int:pk>/edit/', views.edit_song, name='edit_song'),  # Add URL pattern for editing a song
+    path('song/<int:pk>/edit/', views.edit_song, name='edit_song'),
 ]
