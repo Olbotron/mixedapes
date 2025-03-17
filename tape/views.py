c5ea3e38045995eac193c3bea494bab66dfea31a
Olbotron <oliver.g.hughes@gmail.com>
2025-03-17 12:51:55 +0000

    Update static files config

--- tape/views.py
+++ tape/views.py
@@ -1,8 +1,8 @@
-""" from django.shortcuts import render, get_object_or_404, redirect
-from .models import Tape
-from .forms import TapeForm, AddSongToTapeFormSet, DeleteSongsFromTapeForm
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from .models import Tape
 from song.models import Song
+from .forms import TapeForm, AddSongForm, EditSongForm, DeleteSongsForm
 
 def home(request):
     return render(request, 'tape/home.html')
@@ -51,45 +51,6 @@ def tape_delete(request, pk):
         tape.delete()
         return redirect('tape_list')
     return render(request, 'tape/tape_confirm_delete.html', {'tape': tape})
-
-@login_required
-def add_song_to_tape(request, pk):
-    tape = get_object_or_404(Tape, pk=pk)
-    if tape.user != request.user:
-        return redirect('tape_list')  # Redirect if the user is not the owner
-    if request.method == 'POST':
-        formset = AddSongToTapeFormSet(request.POST)
-        if formset.is_valid():
-            for form in formset:
-                title = form.cleaned_data.get('title')
-                artist = form.cleaned_data.get('artist')
-                if title and artist:
-                    Song.objects.create(title=title, artist=artist, tape=tape)
-            return redirect('tape_detail', pk=tape.pk)
-    else:
-        formset = AddSongToTapeFormSet()
-    return render(request, 'tape/add_song_to_tape.html', {'formset': formset, 'tape': tape})
-
-@login_required
-def delete_songs_from_tape(request, pk):
-    tape = get_object_or_404(Tape, pk=pk)
-    if tape.user != request.user:
-        return redirect('tape_list')  # Redirect if the user is not the owner
-    if request.method == 'POST':
-        form = DeleteSongsFromTapeForm(request.POST, tape=tape)
-        if form.is_valid():
-            songs_to_delete = form.cleaned_data['songs']
-            songs_to_delete.delete()
-            return redirect('tape_detail', pk=tape.pk)
-    else:
-        form = DeleteSongsFromTapeForm(tape=tape)
-    return render(request, 'tape/delete_songs_from_tape.html', {'form': form, 'tape': tape}) """
-
-from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib.auth.decorators import login_required
-from .models import Tape
-from song.models import Song
-from .forms import AddSongForm, EditSongForm, DeleteSongsForm
 
 @login_required
 def add_song_to_tape(request, pk):
